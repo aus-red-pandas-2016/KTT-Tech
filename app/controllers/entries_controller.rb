c83d38de3345f088219294a14effc28efd48c382
Torey Hickman <torey@toreyhickman.com>
2015-09-14 18:28:39 -0500

    Write new entry

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -3,3 +3,27 @@ get '/entries' do
 
   erb :'entries/index'
 end
+
+get '/entries/new' do
+  erb :'entries/new'
+end
+
+post '/entries' do
+  @entry = Entry.new(params[:entry])
+
+  if @entry.save
+    redirect "/entries/#{@entry.id}"
+  else
+    @errors = @entry.errors.full_messages
+    erb :'entries/new'
+  end
+end
+
+get '/entries/:id' do
+  begin
+    @entry = Entry.find(params[:id])
+    erb :'entries/show'
+  rescue ActiveRecord::RecordNotFound
+    redirect '/entries'
+  end
+end

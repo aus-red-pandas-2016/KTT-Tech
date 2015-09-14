b10888ea5092baf9bfe34be31e256a8d921b889c
Torey Hickman <torey@toreyhickman.com>
2015-09-14 12:26:02 -0500

    Add seed script

---
+++ db/seeds.rb
@@ -0,0 +1,26 @@
+# Blog entry content courtesy of baconipsum.com.
+
+content_for_entries = [
+  { :title => 'Tenderloin ut Pork Loin',
+    :body  => 'Bacon ipsum dolor amet brisket pastrami non ribeye, exercitation do turducken quis ad flank. Do corned beef ea, consequat ground round minim irure turducken capicola ham kielbasa nisi pork loin tenderloin magna. Ipsum incididunt shankle kevin. Nisi alcatra cupim hamburger pork chop occaecat dolor.' },
+  { :title => 'Sunt Veniam Cow',
+    :body  => 'Doner exercitation picanha excepteur tempor occaecat elit non. Commodo ut spare ribs hamburger esse, nulla fatback. Turducken ground round ipsum, cillum aute meatloaf commodo doner ham hock in kevin alcatra. Bresaola pig quis ullamco dolore reprehenderit flank lorem rump. Pork chop consequat sirloin, salami ullamco kevin veniam dolore dolore. Fugiat venison qui corned beef id pastrami. Esse strip steak filet mignon chuck kevin.' },
+  { :title => 'Cillum Dolor Corned Beef',
+    :body  => 'Est consequat ad. Turducken sint elit irure ut ham hock. Fugiat aute ham salami, shankle occaecat tongue velit bresaola lorem esse ut. Ut proident sed, cupidatat landjaeger swine andouille officia veniam ham hock cupim chuck magna. Mollit fatback adipisicing consequat esse pork belly ground round dolore shankle elit reprehenderit drumstick.' },
+  { :title => 'Prosciutto Cillum Shankle Ball Tip',
+    :body  => 'Andouille biltong shankle tail bacon meatloaf officia, exercitation nisi dolor turkey porchetta. Beef prosciutto tenderloin eu aliquip spare ribs rump sint. In meatloaf minim, ball tip proident aliqua swine meatball enim deserunt velit. Aute labore salami, ea magna ullamco ipsum andouille aliqua. Ham cupim kielbasa shankle turducken corned beef.' },
+  { :title => 'Ut Pancetta Turkey Fatback',
+    :body  => 'Non ham hock nostrud beef ribs biltong alcatra. Nulla bresaola dolore cow eiusmod consequat ribeye salami, picanha sint. Voluptate et ut dolor. Boudin cow venison short loin, in salami voluptate chicken. Do salami frankfurter adipisicing eiusmod, proident alcatra.' },
+  { :title => 'Voluptate Bresaola Porchetta Exercitation',
+    :body  => 'Ipsum tail ea anim, ex leberkas filet mignon dolore in ut est. Sed ground round occaecat, leberkas ham rump ham hock deserunt ad sunt ut meatloaf chuck flank hamburger. Kevin in pork belly cow. Boudin ut occaecat, nulla sirloin leberkas et commodo excepteur. Ut aliquip biltong cillum dolore doner.' },
+  { :title => 'Shankle Shank Nostrud',
+    :body  => 'Consectetur enim pig minim nisi strip steak ad bacon. Ipsum esse flank magna minim. Ex id ullamco brisket short loin. Picanha tongue andouille, dolore sint sirloin consectetur.' },
+  { :title => 'Spare Ribs Velit in Aliqua Ham',
+    :body  => 'Picanha andouille elit pastrami nostrud pork. Salami dolore irure in in drumstick porchetta tongue ex, pork belly cupidatat aliquip ball tip boudin elit. Veniam do pastrami beef, prosciutto short loin non aliquip irure eiusmod sausage salami flank. Et aliqua quis commodo. Porchetta reprehenderit dolore drumstick incididunt adipisicing. Chicken eiusmod adipisicing, flank cupidatat minim landjaeger et sirloin shankle turkey fugiat.' },
+  { :title => 'Pork Chop Cow Duis Consectetur',
+    :body  => 'Aute minim dolor dolore pastrami pig nostrud prosciutto elit. Nisi sed occaecat et, sausage ut beef ad chuck mollit lorem ea. Eu ut short loin cupim ullamco duis strip steak jerky cupidatat tongue beef ribs. Pancetta ground round non leberkas. Ut ullamco sunt pork chop corned beef in salami chuck andouille officia do. Enim ut dolor chuck, pastrami brisket adipisicing corned beef excepteur beef fugiat sed. Tri-tip duis incididunt id.' },
+  { :title => 'Turducken Jowl Pastrami',
+    :body  => 'Venison dolore labore ut flank incididunt turducken beef ribs pariatur non culpa duis. Labore boudin incididunt ex. Exercitation beef nisi hamburger quis nulla cupim. Ad enim meatloaf in consequat. Brisket bacon excepteur sausage do et ball tip leberkas dolore cillum frankfurter ea est occaecat. Pork loin spare ribs corned beef, qui jowl ipsum proident minim biltong incididunt voluptate leberkas boudin anim in.' }
+]
+
+Entry.create!(content_for_entries)

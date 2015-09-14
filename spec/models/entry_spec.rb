614e3514d143143b7f71f31907ab7c1778a6af41
Torey Hickman <torey@toreyhickman.com>
2015-09-14 11:57:13 -0500

    Add entry spec file

---
+++ spec/models/entry_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe Entry do
+  pending "add some examples to (or delete) /Users/Torey/Documents/Dev Bootcamp/editing-challenges/blog-2-multi-author-challenge/Rakefile"
+end

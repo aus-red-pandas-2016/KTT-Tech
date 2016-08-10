ab8440b5bee35da39d103ba7a1f8fe15fe9e2968
Kimberly Patton <email": null>
2016-08-10 15:02:45 -0500

    Update models.

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,3 +1,3 @@
 class Vote < ActiveRecord::Base
-  belongs_to :commentable, polymorphic: true
+  belongs_to :voteable, polymorphic: true
 end
--- db/migrate/20160810145731_create_votes.rb
+++ db/migrate/20160810145731_create_votes.rb
@@ -3,6 +3,8 @@ class CreateVote < ActiveRecord::Migration
     create table :votes do |t|
       t.integer :user_id
       t.integer :value
+      t.integer :voteable_id
+      t.string :voteable_type
       t.timestamps
   end
 end

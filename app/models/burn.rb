72b349f1ebd5db0f9314202e9349cf0d904f5375
Kimberly Patton <email": null>
2016-08-10 14:38:23 -0500

    Create burn migration and model.

---
+++ app/models/burn.rb
@@ -0,0 +1,7 @@
+class Burn < ActiveRecord::Base
+  belongs_to :user
+  has_many :counters
+  has_many :comments
+  has_and_belongs_to :tags
+  has_many :votes, as: :votable_id
+end
---
+++ db/migrate/20160810143110_create_burn.rb
@@ -0,0 +1,8 @@
+class CreateBurn < ActiveRecord::Migration
+  def change
+    create table :burns do |t|
+      t.string :title
+      t.integer :user_id
+      t.text :burn_description
+  end
+end

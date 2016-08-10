53bdcba9cbeb00e660ac678a14febd8fc28fe6d7
Kimberly Patton <email": null>
2016-08-10 18:57:11 -0500

    Create helper for sessions.

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -9,7 +9,7 @@ end
 post '/sign-up' do
   user = User.create(params[:user])
   if user.save
-    session[:user_id] = user.id
+    session[:user] = user
     redirect "/"
    else
      erb :sign_up_form
@@ -23,7 +23,7 @@ end
 post '/login' do
   user = User.find_by(username: params[:username])
   if user.authenticate(params[:password])
-    session[:user_id] = user.id
+    session[:user] = user
     redirect "/"
    else
      #generate errors
@@ -32,7 +32,7 @@ post '/login' do
 end
 
 get '/log-out' do
-  session[:user_id] = nil
+  session[:user] = nil
   redirect '/login'
 end
 

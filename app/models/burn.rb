881ded2e1bf3a90cde6ef87a92f1a259fe939959
Tom Ho <datomster21@gmail.com>
2016-08-10 17:23:55 -0500

    fix more bugs, add depenedent destroys, add seed file

--- app/models/burn.rb
+++ app/models/burn.rb
@@ -1,7 +1,7 @@
 class Burn < ActiveRecord::Base
   belongs_to :user
-  has_many :counters
-  has_many :comments
+  has_many :counters, dependent: :destroy
+  has_many :comments, as: :commentable, dependent: :destroy
   #has_and_belongs_to_many :tags
-  has_many :votes, as: :votable_id
+  has_many :votes, as: :votable, dependent: :destroy
 end

d1e568deb59d07f4bd4cf9707b970dd7cad32e7e
Kimberly Patton <email": null>
2016-08-10 17:01:05 -0500

    Update partial.

--- app/models/burn.rb
+++ app/models/burn.rb
@@ -2,6 +2,6 @@ class Burn < ActiveRecord::Base
   belongs_to :user
   has_many :counters
   has_many :comments
-  has_many_and_belongs_to :tags
+  # has__and_belongs_to_many :tags
   has_many :votes, as: :votable_id
 end

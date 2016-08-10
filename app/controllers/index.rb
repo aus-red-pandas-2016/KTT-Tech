e77fd7c8afc56cba039b2f430f64282eaa6ff0b8
Tolu Oyeniyi <tjoye20@gmail.com>
2016-08-10 15:31:38 -0500

    Add user table and model, add validations, encryption, and sessions.

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,24 +1,37 @@
 get '/' do
-  # @users = User.all
-  erb :index
+  redirect '/burns'
 end
 
 get '/sign-up' do
-
   erb :sign_up_form
 end
 
 post 'sign-up' do
-
-  redirect '/'
+  user = User.create(params[:user])
+  if user.save
+    session[:user_id] = user.id
+    redirect "/"
+   else
+     erb :sign_up_form
+   end
 end
 
 get '/login' do
-
   erb :login_form
 end
 
 post '/login' do
+  user = User.find_by(username: params[:username])
+  if user.authenticate(params[:password])
+    session[:user_id] = user.id
+    redirect "/"
+   else
+     #generate errors
+     erb :login_form
+   end
+end
 
-  redirect '/'
+get '/log-out' do
+  session[:user_id] = nil
+  redirect '/login'
 end

562a460e590af6f8a9c6c915e66b1ca00fe91a37
Tom Ho <datomster21@gmail.com>
2016-08-10 16:14:08 -0500

    fix all models and migrations

--- db/migrate/20160810145731_create_votes.rb
+++ db/migrate/20160810145731_create_votes.rb
@@ -1,10 +1,11 @@
-class CreateVote < ActiveRecord::Migration
+class CreateVotes < ActiveRecord::Migration
   def change
-    create table :votes do |t|
+    create_table :votes do |t|
       t.integer :user_id
       t.integer :value
       t.integer :voteable_id
       t.string :voteable_type
       t.timestamps
+    end
   end
 end

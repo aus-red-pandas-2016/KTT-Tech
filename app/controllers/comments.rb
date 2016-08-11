9ba9bbabc138e167cfe34ef1bd38b65f90385a8f
Kimberly Patton <email": null>
2016-08-11 15:55:11 -0500

    Fix comment posting.

--- app/controllers/comments.rb
+++ app/controllers/comments.rb
@@ -1,11 +1,16 @@
-get '/comments-new' do
+require 'pry'
+get '/comments/new' do
   erb :'comments/new'
 end
 
 post '/comments' do
-  @comment = Comment.new(description: params[:description])
-  if @comment.save
-    redirect "/burns/#{@comment.commentable_id}"
+  @comment = Comment.create(description: params[:comment_description], user_id: current_user.id)
+  binding.pry
+  if request.xhr? && @comment.save
+    status 200
+    erb(:"/comments/_comment", layout: false, locals: {comment: @comment} )
+  elsif request.xhr?
+    status 422
   else
     erb :'comments/new'
   end
--- public/js/application.js
+++ public/js/application.js
@@ -1,34 +1,30 @@
-// $(document).ready(function() {
-//   $("#new_burns").on('submit', fucntion(event){
-//       event.preventDefault();
+$(document).ready(function() {
+  var commentForm = $("#new_comments_form")
+  $("#comments_button").click(function(event){
+    event.preventDefault();
+    $(this).hide()
+    $(commentForm).show();
+  });
 
-//   })
-
-  // $('.burn-list').on('submit', 'form', function(event){
-  //   event.preventDefault();
-  //   var burn_id = $(this).closest('article').attr('id');
-  //   $.ajax({
-  //     url:'/burns/' + burn_id + '/vote',
-  //     method: 'POST',
-  //     dataType: 'json'
-  //     })
-  //   .done(function(result){
-  //     var upvote = result
-  //     $("#" + burn_id +" .points").html(upvote.votes);
-  //     $("#" + burn_id +" button").addClass('voted_up');
-  //   });
-  // });
-
-//   $('.burn-list').on('click', 'a.delete',function(event){
-//     event.preventDefault();
-//     var burn = $(this).closest('article');
-//     var burn_id = burn.attr('id');
-//     $.ajax({
-//         url: '/burns/'+ burn_id,
-//         method: "delete",
-//       })
-//     .done(function(){
-//       post.remove();
-//     });
-//   });
-// });
+  $(commentForm).on('submit', function(event){
+    event.preventDefault();
+    var comment_description = $("textarea[name=description]").val()
+    debugger
+    if (comment_description == ""){
+      alert("You cannot submit an empty comment.");
+        $("#comments_button").show();
+    }else{
+        $.ajax({
+          url:'/comments',
+          method: 'POST',
+          data: {comment_description: comment_description}
+        })
+        .done(function(result){
+          $(".burn-comment-list").append(result);
+          commentForm.trigger("reset")
+          $(commentForm).hide();
+          $("#comments_button").show();
+        });
+    }
+  });
+});

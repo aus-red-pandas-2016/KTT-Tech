6f31b9c3082a68fa25cf75ddec65c3fb826d53ca
Tolu Oyeniyi <tjoye20@gmail.com>
2016-08-11 19:05:04 -0500

    Finish addving counters.

--- public/js/application.js
+++ public/js/application.js
@@ -1,38 +1,3 @@
-// $(document).ready(function() {
-//   $("#new_burns").on('submit', fucntion(event){
-//       event.preventDefault();
-
-//   })
-
-  // $('.burn-list').on('submit', 'form', function(event){
-  //   event.preventDefault();
-  //   var burn_id = $(this).closest('article').attr('id');
-  //   $.ajax({
-  //     url:'/burns/' + burn_id + '/vote',
-  //     method: 'POST',
-  //     dataType: 'json'
-  //     })
-  //   .done(function(result){
-  //     var upvote = result
-  //     $("#" + burn_id +" .points").html(upvote.votes);
-  //     $("#" + burn_id +" button").addClass('voted_up');
-  //   });
-  // });
-
-//   $('.burn-list').on('click', 'a.delete',function(event){
-//     event.preventDefault();
-//     var burn = $(this).closest('article');
-//     var burn_id = burn.attr('id');
-//     $.ajax({
-//         url: '/burns/'+ burn_id,
-//         method: "delete",
-//       })
-//     .done(function(){
-//       post.remove();
-//     });
-//   });
-// });
-
 $(document).ready(function() {
   $("#counters").submit(function(e) {
     e.preventDefault();
@@ -58,8 +23,10 @@ $(document).ready(function() {
       data: {description: counter_description}
     }) //ajax
     .done(function(response) {
-      $(".counter-list").find(".description").last().append(response);
+      $(".counter-list").find("form").last().append(response);
     }); //for done
+    $("#new-counter-form").remove();
+    $(this).find("input[name=description]").val("");
   }); //new-counter-form
 
 }); //document

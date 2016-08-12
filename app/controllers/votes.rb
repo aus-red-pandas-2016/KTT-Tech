ac262459826507ac83e21f448ecb65067e2c69ca
Tom Ho <datomster21@gmail.com>
2016-08-12 08:06:49 -0500

    fix typo in votes controller

--- app/controllers/votes.rb
+++ app/controllers/votes.rb
@@ -2,7 +2,7 @@ post '/burns/:id/voteup' do
   @burn = Burn.find(params[:id])
   @burn.votes.create(value: 1)
   if request.xhr?
-    {votes: @burn.ponits}.to_json
+    {points: @burn.points}.to_json
   else
     redirect "/burns/#{@burn.id}"
   end
@@ -12,7 +12,7 @@ post '/burns/:id/votedown' do
   @burn = Burn.find(params[:id])
   @burn.votes.create(value: -1)
   if request.xhr?
-    {votes: @burn.ponits}.to_json
+    {points: @burn.points}.to_json
   else
     redirect "/burns/#{@burn.id}"
   end
